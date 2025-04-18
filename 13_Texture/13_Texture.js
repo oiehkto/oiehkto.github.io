d3816f18314018c2640b455e6f086f5a4adac003
oiehkto <134214820+oiehkto@users.noreply.github.com>
2025-04-18 23:12:59 +0900

    Add files via upload

---
+++ 13_Texture/13_Texture.js
@@ -0,0 +1,119 @@
+/*-----------------------------------------------------------------------------------
+13_Texture.js
+
+- Viewing a 3D unit cube at origin with perspective projection
+- Rotating the cube by ArcBall interface (by left mouse button dragging)
+- Applying image texture (../images/textures/woodWall2.png) to each face of the cube
+-----------------------------------------------------------------------------------*/
+
+import { resizeAspectRatio, Axes } from '../util/util.js';
+import { Shader, readShaderFile } from '../util/shader.js';
+import { Octahedron } from './regularOctahedron.js';
+import { Arcball } from './arcball.js';
+import { loadTexture } from '../util/texture.js';
+const canvas = document.getElementById('glCanvas');
+const gl = canvas.getContext('webgl2');
+let shader;
+
+let isInitialized = false;
+
+let projMatrix = mat4.create();
+let modelMatrix = mat4.create();
+const axes = new Axes(gl, 1.5); // create an Axes object with the length of axis 1.5
+const texture = loadTexture(gl, true, './sunrise.jpg'); // see ../util/texture.js
+const octahedron = new Octahedron(gl);
+
+// Arcball object: initial distance 5.0, rotation sensitivity 2.0, zoom sensitivity 0.0005
+// default of rotation sensitivity = 1.5, default of zoom sensitivity = 0.001
+const arcball = new Arcball(canvas, 5.0, { rotation: 2.0, zoom: 0.0005 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (isInitialized) {
+        console.log("Already initialized");
+        return;
+    }
+
+    main().then(success => {
+        if (!success) {
+            console.log('program terminated');
+            return;
+        }
+        isInitialized = true;
+    }).catch(error => {
+        console.error('program terminated with error:', error);
+    });
+});
+
+function initWebGL() {
+    if (!gl) {
+        console.error('WebGL 2 is not supported by your browser.');
+        return false;
+    }
+
+    canvas.width = 700;
+    canvas.height = 700;
+    resizeAspectRatio(gl, canvas);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    
+    return true;
+}
+
+async function initShader() {
+    const vertexShaderSource = await readShaderFile('shVert.glsl');
+    const fragmentShaderSource = await readShaderFile('shFrag.glsl');
+    return new Shader(gl, vertexShaderSource, fragmentShaderSource);
+}
+
+function render() {
+    
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    const viewMatrix = arcball.getViewMatrix();
+
+    shader.use();
+    shader.setMat4('u_model', modelMatrix);
+    shader.setMat4('u_view', viewMatrix);
+    shader.setMat4('u_projection', projMatrix);
+
+    octahedron.draw(shader);
+    axes.draw(viewMatrix, projMatrix);
+
+    requestAnimationFrame(render);
+}
+
+async function main() {
+    try {
+        if (!initWebGL()) {
+            throw new Error('WebGL 초기화 실패');
+        }
+        
+        shader = await initShader();
+
+        mat4.rotateY(modelMatrix, modelMatrix, Math.PI / 4);
+
+        mat4.perspective(
+            projMatrix,
+            glMatrix.toRadian(60),  // field of view (fov, degree)
+            canvas.width / canvas.height, // aspect ratio
+            0.1, // near
+            100.0 // far
+        );
+
+        // bind the texture to the shader
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        // call the render function the first time for animation
+        requestAnimationFrame(render);
+
+        return true;
+
+    } catch (error) {
+        console.error('Failed to initialize program:', error);
+        alert('Failed to initialize program');
+        return false;
+    }
+}
+
