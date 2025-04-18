90be4b1bdfda1feb5e11b7435f25a5ec89d2ff69
oiehkto <134214820+oiehkto@users.noreply.github.com>
2025-04-18 23:08:40 +0900

    Update 13_Texture.js

--- 13_Texture/13_Texture.js
+++ 13_Texture/13_Texture.js
@@ -20,7 +20,7 @@ let isInitialized = false;
 let projMatrix = mat4.create();
 let modelMatrix = mat4.create();
 const axes = new Axes(gl, 1.5); // create an Axes object with the length of axis 1.5
-const texture = loadTexture(gl, true, './sunrise.jpg'); // see ../util/texture.js
+const texture = loadTexture(gl, true, 'sunrise.jpg'); // see ../util/texture.js
 const octahedron = new Octahedron(gl);
 
 // Arcball object: initial distance 5.0, rotation sensitivity 2.0, zoom sensitivity 0.0005
