2ea37e4cf5248bc780657455371b01d809340735
oiehkto <134214820+oiehkto@users.noreply.github.com>
2025-04-30 16:48:24 +0900

    Add files via upload

--- 13_Texture/13_Texture.js
+++ 13_Texture/13_Texture.js
@@ -66,7 +66,7 @@ async function initShader() {
 
 function render() {
     
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.1, 0.2, 0.3, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
 
